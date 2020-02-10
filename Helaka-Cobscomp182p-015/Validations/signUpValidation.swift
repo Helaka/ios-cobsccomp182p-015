a05f96c3497afc1c1d584f79fcd9ab1dc5e496e7
Helaka <helaka.jayakody123@gmail.com>
2020-02-11 01:27:30 +0530

    signUpViewController Validations

---
+++ Helaka-Cobscomp182p-015/Validations/signUpValidation.swift
@@ -0,0 +1,30 @@
+//
+//  signUpValidation.swift
+//  Helaka-Cobscomp182p-015
+//
+//  Created by Minu Jayakody on 2/11/20.
+//  Copyright © 2020 Minu Jayakody. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Validations{
+    
+    
+    
+    
+    static func isPasswordValid(_ password : String) -> Bool {
+        
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+    func isValidEmail(email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
+}
